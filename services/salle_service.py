4447a18580de765fee4f13421fe285b26858bae7
rahmataib <rahoumatb20@gmail.com>
2026-04-18 22:44:00 -0400

    Add ServiceSalle class with ajouter_salle method

---
+++ services/salle_service.py
@@ -0,0 +1,9 @@
+from data.dao_salle import DataSalle
+
+class ServiceSalle:
+    def __init__(self):
+        self.dao = DataSalle()
+
+    def ajouter_salle(self, salle):
+        if not all([salle.code, salle.description, salle.categorie, salle.capacite]):
+            return False, "Tous les champs sont obligatoires"
