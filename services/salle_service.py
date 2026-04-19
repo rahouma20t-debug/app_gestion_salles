92f3304c1c7a6b53badcf8dc4edbc7f726183fd2
rahmataib <rahoumatb20@gmail.com>
2026-04-18 22:49:51 -0400

    Add modifier_salle method to ServiceSalle

--- services/salle_service.py
+++ services/salle_service.py
@@ -8,3 +8,15 @@ class ServiceSalle:
         if not all([salle.code, salle.description, salle.categorie, salle.capacite]):
             return False, "Tous les champs sont obligatoires"
 #UPDATE
+        if salle.capacite < 1:
+            return False, "Capacité invalide"
+
+        self.dao.insert_salle(salle)
+        return True, "Salle ajoutée"
+
+    def modifier_salle(self, salle):
+        if salle.capacite < 1:
+            return False, "Capacité invalide"
+
+        self.dao.update_salle(salle)
+        return True, "Salle modifiée"
