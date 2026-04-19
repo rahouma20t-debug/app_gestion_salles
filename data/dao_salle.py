44ab4c688b5d0f9b5d1d77b73aa86766cae2f298
rahmataib <rahoumatb20@gmail.com>
2026-04-18 22:25:00 -0400

    Add get_salle method

--- data/dao_salle.py
+++ data/dao_salle.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import json
+from models.salle import Salle
 
 class DataSalle:
 
@@ -44,3 +45,18 @@ class DataSalle:
 
         conn.commit()
         conn.close()
+
+
+
+    def get_salle(self, code):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM salle WHERE code=%s", (code,))
+        result = cursor.fetchone()
+
+        conn.close()
+
+        if result:
+            return Salle(*result)
+        return None
