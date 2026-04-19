d94167a5c0e7c7eace880ddef6a6588d6fc24f67
rahmataib <rahoumatb20@gmail.com>
2026-04-19 16:09:13 -0400

    create ViewSalle GUI class

--- views/view_salle.py
+++ views/view_salle.py
@@ -8,3 +8,17 @@ class ViewSalle(ctk.CTk):
         self.service = ServiceSalle()
         self.title("Gestion des salles")
         self.geometry("500x500")
+
+
+        self.tree = ttk.Treeview(self, columns=("code", "desc", "cat", "cap"), show="headings")
+
+        self.tree.heading("code", text="Code")
+        self.tree.heading("desc", text="Description")
+        self.tree.heading("cat", text="Categorie")
+        self.tree.heading("cap", text="Capacite")
+
+        self.tree.pack(pady=20)
+
+
+        btn = ctk.CTkButton(self, text="Afficher les salles", command=self.afficher_salles)
+        btn.pack(pady=10)
