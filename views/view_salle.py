f71f701beed3c3c4d28df8762a4bfbbbaf666c0e
rahmataib <rahoumatb20@gmail.com>
2026-04-18 23:07:28 -0400

    create ViewSalle GUI class

--- views/view_salle.py
+++ views/view_salle.py
@@ -2,3 +2,9 @@ import customtkinter as ctk
 from services.salle_service import ServiceSalle
 from models.salle import Salle
 from tkinter import ttk
+class ViewSalle(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.service = ServiceSalle()
+        self.title("Gestion des salles")
+        self.geometry("500x500")
